a934fa41926481b03f3c3c4d4703fdc4b1194dfe
YUE Daian <sheepduke@gmail.com>
2020-06-12 15:57:47 +0800

    feat: implement 2-3

--- src/lib/list.ml
+++ src/lib/list.ml
@@ -1,7 +1,46 @@
-let last (lst: 'a list): ('a option) =
-  let rec doit list = match list with
+(* Write a function last : 'a list -> 'a option
+ * that returns the last element of a list.
+ *
+ * # last [ "a" ; "b" ; "c" ; "d" ];;
+ * - : string option = Some "d"
+ *
+ * # last [];;
+ * - : 'a option = None *)
+let last (list: 'a list): ('a option) =
+  let rec run list = match list with
       [] -> None
     | [x] -> Some x
-    | _::tail -> doit tail
-  in
-  doit lst
+    | _::tail -> run tail
+  in run list
+
+
+(* Find the last but one (last and penultimate) elements of a list.
+ * 
+ * # last_two [ "a" ; "b" ; "c" ; "d" ];;
+ * - : (string * string) option = Some ("c", "d")
+ * 
+ * # last_two [ "a" ];;
+ * - : (string * string) option = None *)
+let last_two (list: 'a list): (('a * 'a) option) =
+  let rec run list = match list with
+      [] -> None
+    | [_] -> None
+    | [x; y] -> Some (x, y)
+    | _::rest -> run rest
+  in run list
+
+
+(* Find the k'th element of a list. (easy)
+ * 
+ * # at 3 [ "a" ; "b"; "c"; "d"; "e" ];;
+ * - : string option = Some "c"
+   * 
+ * # at 3 [ "a" ];;
+ * - : string option = None *)
+let at (index: int) (list: 'a list): 'a option =
+  let rec run index list = match list with
+      [] -> None
+    | _ when index <= 0 -> None
+    | [x] when index == 1 -> Some x
+    | _::rest -> run (index - 1) rest
+  in run index list
--- src/test/test_list.ml
+++ src/test/test_list.ml
@@ -1,11 +1,37 @@
 open O99.List
 open Alcotest
 
+let type_last = (option int)
+let type_last_two = (option (pair int int))
+let type_at = (option int)
+
 let tests = [
   ("last []", `Quick, fun () ->
-      check (option int) "Returns None" None (last ([]: int list)));
+      check type_last "Returns None" None (last []: int option));
   ("last [1]", `Quick, fun () ->
-      check (option int) "Returns Some 1" (Some 1) (last [1]));
+      check type_last "Returns Some 1" (Some 1) (last [1]));
   ("last [1, 2]", `Quick, fun () ->
-      check (option int) "Returns Some 2" (Some 2) (last [1; 2]));
+      check type_last "Returns Some 2" (Some 2) (last [1; 2]));
+
+  ("last_two []", `Quick, fun () ->
+      check type_last_two "Returns None" None (last_two []: (int * int) option));
+  ("last_two [1]", `Quick, fun () ->
+      check type_last_two "Returns None" None (last_two [1]));
+  ("last_two [1, 2]", `Quick, fun () ->
+      check type_last_two "Returns Some 1 2" (Some (1, 2)) (last_two [1; 2]));
+  ("last_two [1, 2, 3]", `Quick, fun () ->
+      check type_last_two "Returns Some 1 2 3" (Some (2, 3)) (last_two [1; 2; 3]));
+
+  ("at 0 []", `Quick, fun () ->
+      check type_at "Returns None" None (at 0 []: int option));
+  ("at 0 [1]", `Quick, fun () ->
+      check type_at "Returns None" None (at 0 [1]));
+  ("at 1 []", `Quick, fun () ->
+      check type_at "Returns None" None (at 1 []: int option));
+  ("at 1 [1]", `Quick, fun () ->
+      check type_at "Returns Some 1" (Some 1) (at 1 [1]));
+  ("at 2 [1]", `Quick, fun () ->
+      check type_at "Returns None" None (at 2 [1]));
+  ("at 2 [1, 2]", `Quick, fun () ->
+      check type_at "Returns Some 2" (Some 2) (at 2 [1; 2]));
 ]

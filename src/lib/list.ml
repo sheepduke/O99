31528189a4e89a951ceba6e50d2622884cc41659
YUE Daian <sheepduke@gmail.com>
2020-06-13 19:26:05 +0800

    feat: implement 4-7

--- src/lib/list.ml
+++ src/lib/list.ml
@@ -1,46 +1,100 @@
-(* Write a function last : 'a list -> 'a option
- * that returns the last element of a list.
- *
- * # last [ "a" ; "b" ; "c" ; "d" ];;
- * - : string option = Some "d"
- *
- * # last [];;
- * - : 'a option = None *)
-let last (list: 'a list): ('a option) =
-  let rec run list = match list with
-      [] -> None
-    | [x] -> Some x
-    | _::tail -> run tail
-  in run list
+(**
+ ** 1. Write a function last : 'a list -> 'a option that returns the last element of a list. (easy)
+ **
+ ** # last [ "a" ; "b" ; "c" ; "d" ];;
+ ** - : string option = Some "d"
+ **
+ ** # last [];;
+ ** - : 'a option = None *)
+let rec last (list : 'a list) : ('a option) =
+  match list with
+    [] -> None
+  | [x] -> Some x
+  | _::tail -> last tail
 
+(**
+ ** 2. Find the last but one (last and penultimate) elements of a list. (easy)
+ ** 
+ ** # last_two [ "a" ; "b" ; "c" ; "d" ];;
+ ** - : (string * string) option = Some ("c", "d")
+ ** 
+ ** # last_two [ "a" ];;
+ ** - : (string * string) option = None *)
+let rec last_two (list : 'a list) : (('a * 'a) option) =
+  match list with
+    [] -> None
+  | [_] -> None
+  | [x; y] -> Some (x, y)
+  | _::rest -> last_two rest
 
-(* Find the last but one (last and penultimate) elements of a list.
- * 
- * # last_two [ "a" ; "b" ; "c" ; "d" ];;
- * - : (string * string) option = Some ("c", "d")
- * 
- * # last_two [ "a" ];;
- * - : (string * string) option = None *)
-let last_two (list: 'a list): (('a * 'a) option) =
-  let rec run list = match list with
-      [] -> None
-    | [_] -> None
-    | [x; y] -> Some (x, y)
-    | _::rest -> run rest
-  in run list
+(**
+ ** 3. Find the k'th element of a list. (easy)
+ ** 
+ ** # at 3 [ "a" ; "b"; "c"; "d"; "e" ];;
+ ** - : string option = Some "c"
+ ** 
+ ** # at 3 [ "a" ];;
+ ** - : string option = None *)
+let rec at (index : int) (list : 'a list) : 'a option =
+  match list with
+    [] -> None
+  | _ when index <= 0 -> None
+  | [x] when index == 1 -> Some x
+  | _::rest -> at (index - 1) rest
 
+(**
+ ** 4. Find the number of elements of a list. (easy)
+ ** OCaml standard library has List.length but we ask that you reimplement it. Bonus for a tail recursive solution.
+ **
+ ** # length [ "a" ; "b" ; "c"];;
+ ** - : int = 3
+ ** # length [];;
+ ** - : int = 0 *)
+let length (list : 'a list) : int =
+  let rec run len list =
+    match list with
+      [] -> len
+    | _ :: rest -> run (len + 1) rest
+  in run 0 list
 
-(* Find the k'th element of a list. (easy)
- * 
- * # at 3 [ "a" ; "b"; "c"; "d"; "e" ];;
- * - : string option = Some "c"
-   * 
- * # at 3 [ "a" ];;
- * - : string option = None *)
-let at (index: int) (list: 'a list): 'a option =
-  let rec run index list = match list with
-      [] -> None
-    | _ when index <= 0 -> None
-    | [x] when index == 1 -> Some x
-    | _::rest -> run (index - 1) rest
-  in run index list
+(**
+ ** 5. Reverse a list. (easy)
+ ** OCaml standard library has List.rev but we ask that you reimplement it.
+ **
+ ** # rev ["a" ; "b" ; "c"];;
+ ** - : string list = ["c"; "b"; "a"] *)
+let rev (list : 'a list) : 'a list =
+  let rec run source result =
+    match source with
+      [] -> result
+    | head :: tail -> run tail (head :: result) in
+  run list []
+
+(**
+ ** 6. Find out whether a list is a palindrome. (easy)
+ ** HINT: a palindrome is its own reverse.
+ **
+ ** # is_palindrome [ "x" ; "a" ; "m" ; "a" ; "x" ];;
+ ** - : bool = true
+ ** # not (is_palindrome [ "a" ; "b" ]);;
+ ** - : bool = true *)
+let is_palidrome (list : 'a list) =
+  list = rev list
+
+(**
+ ** 7. Flatten a nested list structure. (medium)
+ ** 
+ ** There is no nested list type in OCaml, so we need to define one
+     first. A node of a nested list is either an element, or a list of
+     nodes.
+ ** # flatten [ One "a" ; Many [ One "b" ; Many [ One "c" ; One "d" ] ; One "e" ] ];;
+ ** - : string list = ["a"; "b"; "c"; "d"; "e"] *)
+type 'a node =
+  | One of 'a 
+  | Many of 'a node list
+
+let rec flatten (list : 'a node list) : 'a list =
+  match list with
+    [] -> []
+  | One head :: tail -> head :: flatten tail
+  | Many head_list :: tail -> flatten head_list @ flatten tail
--- src/test/test_list.ml
+++ src/test/test_list.ml
@@ -1,37 +1,65 @@
 open O99.List
 open Alcotest
 
-let type_last = (option int)
-let type_last_two = (option (pair int int))
-let type_at = (option int)
+let option_int = (option int)
+let option_int_2 = (option (pair int int))
 
 let tests = [
   ("last []", `Quick, fun () ->
-      check type_last "Returns None" None (last []: int option));
+      check option_int "Returns None" None (last []: int option));
   ("last [1]", `Quick, fun () ->
-      check type_last "Returns Some 1" (Some 1) (last [1]));
+      check option_int "Returns Some 1" (Some 1) (last [1]));
   ("last [1, 2]", `Quick, fun () ->
-      check type_last "Returns Some 2" (Some 2) (last [1; 2]));
+      check option_int "Returns Some 2" (Some 2) (last [1; 2]));
 
   ("last_two []", `Quick, fun () ->
-      check type_last_two "Returns None" None (last_two []: (int * int) option));
+      check option_int_2 "Returns None" None (last_two []: (int * int) option));
   ("last_two [1]", `Quick, fun () ->
-      check type_last_two "Returns None" None (last_two [1]));
+      check option_int_2 "Returns None" None (last_two [1]));
   ("last_two [1, 2]", `Quick, fun () ->
-      check type_last_two "Returns Some 1 2" (Some (1, 2)) (last_two [1; 2]));
+      check option_int_2 "Returns Some 1 2" (Some (1, 2)) (last_two [1; 2]));
   ("last_two [1, 2, 3]", `Quick, fun () ->
-      check type_last_two "Returns Some 1 2 3" (Some (2, 3)) (last_two [1; 2; 3]));
+      check option_int_2 "Returns Some 1 2 3" (Some (2, 3)) (last_two [1; 2; 3]));
 
   ("at 0 []", `Quick, fun () ->
-      check type_at "Returns None" None (at 0 []: int option));
+      check option_int "Returns None" None (at 0 []: int option));
   ("at 0 [1]", `Quick, fun () ->
-      check type_at "Returns None" None (at 0 [1]));
+      check option_int "Returns None" None (at 0 [1]));
   ("at 1 []", `Quick, fun () ->
-      check type_at "Returns None" None (at 1 []: int option));
+      check option_int "Returns None" None (at 1 []: int option));
   ("at 1 [1]", `Quick, fun () ->
-      check type_at "Returns Some 1" (Some 1) (at 1 [1]));
+      check option_int "Returns Some 1" (Some 1) (at 1 [1]));
   ("at 2 [1]", `Quick, fun () ->
-      check type_at "Returns None" None (at 2 [1]));
+      check option_int "Returns None" None (at 2 [1]));
   ("at 2 [1, 2]", `Quick, fun () ->
-      check type_at "Returns Some 2" (Some 2) (at 2 [1; 2]));
+      check option_int "Returns Some 2" (Some 2) (at 2 [1; 2]));
+
+  ("length []", `Quick, fun () -> check int "Returns 0" 0 (length []));
+  ("length [1]", `Quick, fun () -> check int "Returns 1" 1 (length [1]));
+  ("length [1; 2]", `Quick, fun () -> check int "Returns 2" 2 (length [1; 2]));
+
+  ("rev []", `Quick, fun () ->
+      check (list int) "Returns []" [] (rev []));
+  ("rev [1]", `Quick, fun () ->
+      check (list int) "Returns [1]" [1] (rev [1]));
+  ("rev [1; 2]", `Quick, fun () ->
+      check (list int) "Returns [2; 1]" [2; 1] (rev [1; 2]));
+
+  ("is_palidrome []", `Quick, fun () ->
+      check bool "Returns true" true (is_palidrome []));
+  ("is_palidrome [1]", `Quick, fun () ->
+      check bool "Returns true" true (is_palidrome [1]));
+  ("is_palidrome [1; 2; 1]", `Quick, fun () ->
+      check bool "Returns true" true (is_palidrome [1; 2; 1]));
+  ("is_palidrome [1; 2]", `Quick, fun () ->
+      check bool "Returns false" false (is_palidrome [1; 2]));
+
+  ("flatten []", `Quick, fun () ->
+      check (list int) "Returns []" [] (flatten []));
+  ("flatten [One 'a']", `Quick, fun () ->
+      check (list char) "Returns ['a']" ['a'] (flatten [One 'a']));
+  ("flatten [One 'a'; Many[One 'b'; Many[One 'c'; One 'd']; One 'e']]", `Quick,
+   fun () -> check (list char) "Returns ['a'; 'b'; 'c'; 'd'; 'e']"
+       ['a'; 'b'; 'c'; 'd'; 'e']
+       (flatten [One 'a'; Many[One 'b'; Many[One 'c'; One 'd']; One 'e']]));
 ]
